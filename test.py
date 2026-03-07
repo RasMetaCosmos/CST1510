425468aaaaeb1c1bc3cb70c6e1bf8de34e2842fb
RasMetaCosmos <rashik100dallakoti@gmail.com>
2026-03-07 13:18:06 +0000

    bycrypt practice

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
-print ("AWAKE!!!")
-print ("Rashik---$$$")
-print ("Life is a paradox!!!")
+import bcrypt
+
+def hash_password(plain_text_password):
+    password_bytes = plain_text_password.encode("utf-8")
+    salt= bcrypt.gensalt()
+    bcrypted_password=bcrypt.hashpw(password_bytes, salt)
+    return(bcrypted_password).decode("utf-8")
+
+
+
+def check_password(plain_text_password,hash_password):
+    password_bytes = plain_text_password.encode("utf-8")
+    hash_password_bytes = hash_password.encode("utf-8")
+    if bcrypt.checkpw(password_bytes,hash_password_bytes):
+        print("Password is correct")
+    else:
+        print("Password is incorrect")
+
+user_name = input("Enter your name")
+password = input("Enter your password")
+
+h_password=hash_password(password)
+print(f"Your hashed password is: {h_password}")
+with open("users.txt","a") as f:
+    f.write(f"{user_name},{h_password}\n")
